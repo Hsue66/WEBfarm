583d1c4d63d851497319d2959b387c8035b232cd
Son Ung Bea <thsdndqo00@naver.com>
2015-12-18 20:21:35 +0900

    웅) 관리창 완성직전 수민이가 사진 경로 봐줘야함!
    11

---
+++ manage.php
@@ -0,0 +1,29 @@
+<?
+    $men=0;
+    $userpic=0;
+    $farmpic=0;
+    $fnum = $_POST['farmnum'];
+    $conn = mysql_connect('localhost', 'root', 'apmsetup');
+    mysql_query("set names utf8");
+    if(!$conn)
+    {
+        echo "Fail";
+    }
+    $db_name="webfarm";
+    mysql_select_db($db_name, $conn);
+
+    $men=$_POST['mention'];
+    $userpic=$_POST['userpic'];
+    $farmpic=$_POST['farmpic'];
+
+
+        $que1=mysql_query("UPDATE farm set MENTION='$men' where fnum='$fnum'");
+ 
+  
+        $que2=mysql_query("UPDATE farm set FPHOTO='$farmpic' where fnum='$fnum'");
+   
+        $que3=mysql_query("UPDATE user INNER JOIN farm ON (user.userID=farm.userID) set UPHOTO='$userpic' WHERE fnum=$fnum");
+   
+
+    echo "<script>location.href='Farmmanage.html?farmnum=$fnum';</script>"; 
+?>
