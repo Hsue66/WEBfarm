561041de9e9707f01b3ddeb2b1e69a488b0ed177
dufmaakedl@gmail.com <dufmaakedl@naver.com>
2015-12-18 21:49:39 +0900

    카드저장 완료!

--- add_cart.php
+++ add_cart.php
@@ -1,7 +1,20 @@
+<? include "lib.php" ?>
 <?
 header("Content-Type: text/html; charset= UTF-8 ");
 
  include "./connect_db.php"; // 데이터 베이스 접속 프로그램 불러오기
+
+	if(!isset($_SESSION["id"])){
+   echo "<script> alert('로그인 하셔야 이용 가능합니다.');</script>";
+   echo "<script language='javascript'>location.replace('login.html');";
+   echo "</script>";
+	}
+
+	else if(trim($_GET['count']) == ""){
+        echo "<script> alert('수량을 입력하시지 않았습니다.'); history.back();</script>";
+	}
+
+	else{
    $pnum = $_GET['pnum'];
    $id = $_GET['id'];
    $count = $_GET['count'];
@@ -9,6 +22,6 @@ header("Content-Type: text/html; charset= UTF-8 ");
    mysql_query($save);
 
    echo '<script>alert("장바구니에 저장되었습니다!");history.back();</script>';
-   
+   }
 
 ?> 
