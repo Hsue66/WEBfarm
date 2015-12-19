3cf2439bfe6becd680d1ed528ba60365de5fa6b6
dufmaakedl@gmail.com <dufmaakedl@naver.com>
2015-12-19 15:14:00 +0900

    마이페이지-물건정보

--- add_cart.php
+++ add_cart.php
@@ -18,7 +18,7 @@ header("Content-Type: text/html; charset= UTF-8 ");
    $pnum = $_GET['pnum'];
    $id = $_GET['id'];
    $count = $_GET['count'];
-   $save = "insert into CART (USERID, PNUM, PCOUNT) values ('".$id."', '".$pnum."', '".$count."')";
+   $save = "insert into CART (USERID, PNUM, COUNT) values ('".$id."', '".$pnum."', '".$count."')";
    mysql_query($save);
 
    echo '<script>alert("장바구니에 저장되었습니다!");history.back();</script>';
