f352e108049cf5a9a290cab3ec1d935f754a10e8
dufmaakedl@gmail.com <dufmaakedl@naver.com>
2015-12-20 10:08:43 +0900

    이제좀 먼가괜찬

--- slidebannerthr/js/script.js
+++ slidebannerthr/js/script.js
@@ -14,12 +14,12 @@
     //두번째 배너
     $( "#image_list_2" ).jQBanner( {
                                     //롤링을 할 영역의 ID 값
-        nWidth: 904,				//영역의 width
-        nHeight: 456,				//영역의 height
+        nWidth: 900,				//영역의 width
+        nHeight: 400,				//영역의 height
         nCount: 5,					//돌아갈 이미지 개수
         isActType: "right",			//움직일 방향 (left, right, up, down)
         nOrderNo: 1,				//초기 이미지
-        nDelay: 2000				//롤링 시간 타임 (1000 = 1초)
+        nDelay: 4000				//롤링 시간 타임 (1000 = 1초)
         
     } );
     //세번째 배너
